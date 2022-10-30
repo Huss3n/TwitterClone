ea07224eb9ff897ccd8e3abbe4b9b2f2f7acf9bc
Huss3n <85851097+Huss3n@users.noreply.github.com>
2022-10-30 12:22:45 +0300

    Initial Commit

---
+++ TwitterClone/TwitterCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwitterCloneApp.swift
+//  TwitterClone
+//
+//  Created by Muktar Hussein on 30/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct TwitterCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
